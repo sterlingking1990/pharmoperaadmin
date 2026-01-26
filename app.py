3e24763abb9eb2fab50d35f2bbea942d59f2fe70
Izundu Kingsley <izundukingsleyemeka@gmail.com>
2026-01-26 07:34:31 +0100

    uncheck dev server

--- app.py
+++ app.py
@@ -927,7 +927,7 @@ if __name__ == '__main__':
     mcp_thread.start()
 
     print("Starting PharmOpera Admin server...")
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False, allow_unsafe_werkzeug=False)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False, allow_unsafe_werkzeug=True)
 
 # Export for Vercel
 handler = app
